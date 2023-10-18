0c9e8e2e431af49d0764e1809fa48ea6d02955e4
yassarmed <yassarahmed17@gmail.com>
2023-10-18 13:38:35 -0500

    creates function which allows movement of player sprite

--- fighters.js
+++ fighters.js
@@ -21,6 +21,8 @@ class Sprite {
 
     update() {
         this.draw()
+        
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -70,5 +72,19 @@ function animate() {
 animate()
 
 window.addEventListener('keydown', (event)=>{
-    console.log(event)
+    switch (event.key) {
+        case 'd':
+        player.velocity.x = 1
+        break
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch (event.key) {
+        case 'd':
+        player.velocity.x = 0
+        break
+    }
+    console.log(event.key)
 })
