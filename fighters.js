9fc0496301dca2cfb0bc1cd1ccd4eccd380385e1
yassarmed <yassarahmed17@gmail.com>
2023-10-19 15:32:01 -0500

    gives player ability to jump

--- fighters.js
+++ fighters.js
@@ -60,6 +60,20 @@ const enemy = new Sprite({
 
 console.log(player)
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    }
+}
+
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -67,6 +81,13 @@ function animate() {
     player.update()
     enemy.update()
 
+    player.velocity.x = 0
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd'){
+        player.velocity.x = 1
+    }
 }
 
 animate()
@@ -74,7 +95,15 @@ animate()
 window.addEventListener('keydown', (event)=>{
     switch (event.key) {
         case 'd':
-        player.velocity.x = 1
+        keys.d.pressed = true
+        lastKey = 'd'
+        break
+        case 'a':
+        keys.a.pressed = true
+        lastKey = 'a'
+        break
+        case 'w':
+       player.velocity.y = -10
         break
     }
     console.log(event.key)
@@ -83,7 +112,13 @@ window.addEventListener('keydown', (event)=>{
 window.addEventListener('keyup', (event)=>{
     switch (event.key) {
         case 'd':
-        player.velocity.x = 0
+       keys.d.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+        case 'w':
+        keys.w.pressed = false
         break
     }
     console.log(event.key)
