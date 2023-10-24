c21b6854d005a4944d59cddb3a8c7809d6375e60
yassarmed <yassarahmed17@gmail.com>
2023-10-24 13:14:21 -0500

    creates hitboxs and color codes enemy and player

--- fighters.js
+++ fighters.js
@@ -8,17 +8,29 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'blue'}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
         this.lastKey 
+        this.attackBox = {
+            position:this.position ,
+            width:100 ,
+            height:50 ,
+        }
+        this.color = color
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, 50, this.height )
+
+        c.fillStyle = 'yellow'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height )
     }
+
+    //attack box
+   
 
     update() {
         this.draw()
@@ -54,7 +66,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0,
-    }
+    },
+    color: 'red'
 
 })
  
